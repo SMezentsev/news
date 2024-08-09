c3165f89391a0838aa1f2cae5b08bcb9eb791604
SMezentsev <kurt_dc@mail.ru>
2024-08-09 12:33:58 +0300

    fiz date in 380 article post

--- frontend/views/articles/_article_380.php
+++ frontend/views/articles/_article_380.php
@@ -12,8 +12,13 @@ use Carbon\Carbon;
   </div>
   <div class="pl-10 pr-10">
     <h5 class="post-title mb-15"><a href="single.html"><?= $news->announce??'' ?></a></h5>
-    <div class="entry-meta meta-1 font-x-small color-grey float-left text-uppercase mb-10">
-      <span class="post-on"><?= Carbon::parse($news->date)->format('d-m-y'); ?></span>
+    <div class="entry-meta meta-1 font-x-small color-grey float-left mb-10">
+      <span class="post-on">
+        <?= Carbon::parse($news->date)->format('H:i, '); ?>
+        <?= intval(Carbon::parse($news->date)->format('d')); ?>
+        <?= DateHelper::months(Carbon::parse($news->date)->format('m'), false) ?>
+        <?= Carbon::parse($news->date)->format('y'); ?>
+      </span>
     </div>
   </div>
 </article>
