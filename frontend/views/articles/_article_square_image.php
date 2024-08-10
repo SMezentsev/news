b1d7b4d4f9d76cc6f58920cc0177c7737c050236
SMezentsev <kurt_dc@mail.ru>
2024-08-10 08:54:05 +0300

    fix news main apge

--- frontend/views/articles/_article_square_image.php
+++ frontend/views/articles/_article_square_image.php
@@ -14,11 +14,11 @@ use common\Helper\DateHelper;
     <div class="post-content media-body">
       <div class="entry-meta mb-5 mt-5">
         <a class="entry-meta meta-2" href="<?= '/news/'.$news->category_id ?>"><span
-            class="post-in text-danger font-x-small"><?= $news->category->name; ?></span></a>
+            class="post-in text-danger font-x-small"><?= $news->category->name??''; ?></span></a>
       </div>
       <h5 class="post-title mb-15 text-limit-2-row">
         <span class="post-format-icon">
-        <?= $news->category->icon; ?>
+        <?= $news->category->icon??''; ?>
         </span>
         <a href="<?= '/news/'.$news->category_id.'/'.$news->id ?>"><?= $news->title; ?></a></h5>
       <div class="entry-meta meta-1 font-x-small color-grey float-left">
