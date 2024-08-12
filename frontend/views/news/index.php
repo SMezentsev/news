9adde6e3c83fc5480c7dee2e2a480160d886b4c7
SMezentsev <kurt_dc@mail.ru>
2024-08-12 17:07:51 +0300

    fix category nav sort

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -6,10 +6,10 @@ use Carbon\Carbon;
 
 $category = NewsCategory::find()->where(['id' => $category_id])->one();
 if($category->eng_name == 'all') {
-  $parent = NewsCategory::find()->where(['id' => $category->id])->orderBy('data')->one();
+  $parent = NewsCategory::find()->where(['id' => $category->id])->orderBy('date')->one();
   $parent_id = $parent->id;
 } else {
-  $parent = NewsCategory::find()->where(['id' => $category->parent_id])->orderBy('data')->one();
+  $parent = NewsCategory::find()->where(['id' => $category->parent_id])->orderBy('date')->one();
   $parent_id = $category->parent_id;
 }
 
