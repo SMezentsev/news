eaac354a6dc5bed00c1ff28e91b201d598247f4f
SMezentsev <kurt_dc@mail.ru>
2024-08-27 19:51:58 +0300

    add keywords on page

--- frontend/controllers/NewsController.php
+++ frontend/controllers/NewsController.php
@@ -69,9 +69,12 @@ class NewsController extends Controller {
 
         $newsCycles = $newsModel->search(['notIn' => [$model->id], 'news_cycle_id' => $model->news_cycle_id]);
       }
-      $this->view->title = $category->name.': '.$model->title;
-      $this->view->registerMetaTag(['name' => 'keywords', 'content' => $category->name.' '.$model->title]);
-      $this->view->registerMetaTag(['name' => 'description', 'content' => $category->name.' '.$model->announce]);
+      //$this->view->title = $category->name.': '.$model->title;
+//      $this->view->registerMetaTag(['name' => 'keywords', 'content' => $category->name.' '.$model->title]);
+//      $this->view->registerMetaTag(['name' => 'description', 'content' => $category->name.' '.$model->announce]);
+      $this->view->title = $model->keywords->meta_tag_keywords??$category->name.': '.$model->title;
+      $this->view->registerMetaTag(['name' => 'keywords', 'content' => $model->keywords->meta_tag_keywords??'']);
+      $this->view->registerMetaTag(['name' => 'description', 'content' => $model->keywords->meta_tag_description??'']);
 
       return $this->render('view', [
         'breadCrumbs' => $breadCrumbs,
